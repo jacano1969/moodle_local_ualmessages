32a4ec0e2b152aaea4b16206bf96cb4b0b59913a
Daniel Somers <d.somers@ulcc.ac.uk>
2012-12-21 09:07:22 +0000

    removed unrequired div

--- renderer.php
+++ renderer.php
@@ -627,7 +627,11 @@ TEMP */
             $isuserblocked = null;
         
             if ($countonlinecontacts + $countofflinecontacts == 0) {
-                $content .= html_writer::tag('div', get_string('contactlistempty', 'message'), array('class' => 'heading'));
+                $content .= html_writer::start_tag('ul');
+                $content .= html_writer::start_tag('li');
+                $content .= get_string('contactlistempty', 'message');
+                $content .= html_writer::end_tag('li');
+                $content .= html_writer::end_tag('ul');
             }
         
             $content .= html_writer::start_tag('table', array('id' => 'message_contacts', 'class' => 'boxaligncenter'));
