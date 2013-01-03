45f990d37a524323ae0dfb51290ba760416e0462
Daniel Somers <d.somers@ulcc.ac.uk>
2013-01-03 09:50:07 +0000

    addded css class

--- renderer.php
+++ renderer.php
@@ -264,7 +264,7 @@ TEMP */
         $content .= get_string('filterbycourse', 'local_ualmessages');
         $content .= html_writer::end_tag('label');
         $content .= html_writer::select($options, 'viewing', $viewing, false, array('id' => 'viewing','onchange' => 'this.form.submit()'));
-        $content .= html_writer::tag('input', '', array('type'=>'text', 'id'=>'msgsearchname', 'name'=>'msgsearchname'));
+        $content .= html_writer::tag('input', '', array('type'=>'text', 'class'=>'nolabel' ,'id'=>'msgsearchname', 'name'=>'msgsearchname'));
         $content .= html_writer::end_tag('p');
         $content .= html_writer::end_tag('form');
         $content .= html_writer::end_tag('div');
