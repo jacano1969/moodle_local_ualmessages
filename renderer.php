5a41d925e4e75e068226649020e5c795e85d7daa
Daniel Somers <d.somers@ulcc.ac.uk>
2013-01-03 09:39:13 +0000

    paragraph tag

--- renderer.php
+++ renderer.php
@@ -265,10 +265,12 @@ TEMP */
         $content .= html_writer::end_tag('label');
         $content .= html_writer::select($options, 'viewing', $viewing, false, array('id' => 'viewing','onchange' => 'this.form.submit()'));
         $content .= html_writer::end_tag('p');
+        $content .= html_writer::start_tag('p');
         $content .= html_writer::start_tag('label');
         $content .= ' ';
         $content .= html_writer::end_tag('label');
         $content .= html_writer::tag('input', '', array('type'=>'text', 'id'=>'msgsearchname', 'name'=>'msgsearchname'));
+        $content .= html_writer::end_tag('p');
         $content .= html_writer::end_tag('form');
         $content .= html_writer::end_tag('div');
                        
