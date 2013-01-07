224b0d037e41eb35cc7733528a3905c6fdefa1d1
Daniel Somers <d.somers@ulcc.ac.uk>
2013-01-07 09:31:45 +0000

    updated js

--- script/viewpaging.js
+++ script/viewpaging.js
@@ -4,12 +4,16 @@ var ualmessagespaging = ualmessagespaging || (function(){
    
     "use strict";
     
-$('div.paging a').each(function() {   
-    var viewed = $('#viewing').val(); 
-    var _href = $(this).attr("href");
-    var newhref = _href + '&viewing=' + viewed;
-    $(this).attr("href", newhref); 
-});
-
+    $(document).ready(function(){
+        
+    $('div.paging a').each(function() {   
+        var viewed = $('#viewing').val(); 
+        var _href = $(this).attr("href");
+        var newhref = _href + '&viewing=' + viewed;
+        $(this).attr("href", newhref); 
+    });
+    
+    });
+    
 }());
 
