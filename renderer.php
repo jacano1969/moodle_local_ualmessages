17e8ab3e876adc6a07ccd9754971c5d1e547edfc
Daniel Somers <d.somers@ulcc.ac.uk>
2013-01-11 13:06:18 +0000

    hide contact search boc if no course

--- renderer.php
+++ renderer.php
@@ -570,14 +570,23 @@ TEMP */
         $content .= html_writer::end_tag('label');
         $content .= html_writer::select($options, 'viewing', $viewing, false, array('id' => 'viewing','onchange' => 'this.form.search.value=\'\';this.form.submit()'));
         $content .= html_writer::end_tag('p');
-        $content .= html_writer::start_tag('p');
-        $content .= html_writer::start_tag('label');
-        $content .= get_string('searchcontacts', 'local_ualmessages');
-        $content .= html_writer::end_tag('label');
         
-        $content .= html_writer::start_tag('input', array('name'=>'search', 'value'=> ($search=='') ? get_string('searchcontacts', 'local_ualmessages') : $search,'class'=>'text','onclick'=>'this.form.search.value=\'\';'));
-        $content .= html_writer::start_tag('input', array('type'=>'submit','name'=>'','value'=>get_string('search', 'local_ualmessages'),'class'=>'submit','onclick'=>'if(this.form.search.value==\'\' || this.form.search.value==\''.get_string('searchcontacts', 'local_ualmessages').'\'){return false;}'));  
-        $content .= html_writer::end_tag('p');
+        
+        // get course participant contacts - using the filter
+        $course_id = intval(substr($viewing, 7));
+        
+        
+        $content .= html_writer::start_tag('p');
+                    
+        if(!empty($course_id)) {
+            $content .= html_writer::start_tag('label');
+            $content .= get_string('searchcontacts', 'local_ualmessages');
+            $content .= html_writer::end_tag('label');
+            $content .= html_writer::start_tag('input', array('name'=>'search', 'value'=> ($search=='') ? get_string('searchcontacts', 'local_ualmessages') : $search,'class'=>'text','onclick'=>'this.form.search.value=\'\';'));
+            $content .= html_writer::start_tag('input', array('type'=>'submit','name'=>'','value'=>get_string('search', 'local_ualmessages'),'class'=>'submit','onclick'=>'if(this.form.search.value==\'\' || this.form.search.value==\''.get_string('searchcontacts', 'local_ualmessages').'\'){return false;}'));  
+            $content .= html_writer::end_tag('p');
+        }
+        
         $content .= html_writer::end_tag('form');
         $content .= html_writer::end_tag('div');
                        
@@ -587,8 +596,6 @@ TEMP */
         // prepare search
         $search = stripcslashes(clean_text(trim($search)));
         
-        // get course participant contacts - using the filter
-        $course_id = intval(substr($viewing, 7));
         if(!empty($course_id)) {
             
             $countparticipants = count_enrolled_users($coursecontexts[$course_id]);
